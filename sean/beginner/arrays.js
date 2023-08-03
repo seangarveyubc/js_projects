2ea06219ee9429286c47a890d2ab23c3d6b8e05d
Sean Garvey <sgarveybc@gmail.com>
2023-08-02 21:31:01 -0700

    new file structure

---
+++ sean/beginner/arrays.js
@@ -0,0 +1,70 @@
+/**
+ * 
+ * This file will house code pertaining to arrays, and algorithms that perform CRUD operations on them
+ * 
+ */
+
+/**
+ * Attributes:
+ *  arr.length
+ *  arr.prototype
+ * Methods
+ *  arr.at(val)
+ *  arr.concat(arr2)
+ *  arr.copyWithin()
+ *  arr.entries()
+ *  arr.every()
+ *  arr.fill()
+ *  arr.filter()
+ *  arr.find()
+ *  arr.findIndex()
+ *  arr.findLast()
+ *  arr.findLastIndex()
+ *  arr.flat()
+ *  arr.flatMap()
+ *  arr.forEach()
+ *  arr.from()
+ *  arr.fromAsync()
+ *  arr.group()
+ *  arr.groupToMap()
+ *  arr.includes()
+ *  arr.indexOf()
+ *  arr.isArray()
+ *  arr.join()
+ *  arr.keys()
+ *  arr.lastIndexOf()
+ *  arr.map()
+ *  arr.of()
+ *  arr.pop()
+ *  arr.push()
+ *  arr.reduce()
+ *  arr.reduceRight()
+ *  arr.reverse()
+ *  arr.shift()
+ *  arr.slice()
+ *  arr.some()
+ *  arr.sort()
+ *  arr.splice()
+ *  arr.toLocaleString()
+ *  arr.toReversed()
+ *  arr.toSorted()
+ *  arr.toSpliced()
+ *  arr.toString()
+ *  arr.unshift()
+ *  arr.values()
+ *  arr.with()
+ */
+
+// This section of code will be dedicated to the insertion of elements into arrays of different types
+
+
+var oceans = [
+    'Atlantic',
+    'Pacific'
+]
+console.log();
+oceans.push('Indian'); //inserting 1 elements
+oceans.push('Antarctic', 'Southern'); //inserting multiple
+
+
+
